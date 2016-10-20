06b543983afb7fade6f72b9a67fda0990f6b6447
raihan02 <raihanraju02@gmail.com>
2016-10-20 12:18:11 +0600

    Update Merge_Sort.cpp

--- Merge_Sort.cpp
+++ Merge_Sort.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#define inf 1 << 30
+#define inf 1 << 30 /// Infenite value 
 using namespace std;
-int A[1001];
+int A[1001]; /// Global Array declare 
 int n;
 void merges(int p , int q, int r)
 {
-    int n1=  q -p + 1;
-    int n2 = r - q;
-    int L[n1 + 1] , R[n2 + 1];
+    int n1=  q -p + 1; /// First array length
+    int n2 = r - q; /// second Array length
+    int L[n1 + 1] , R[n2 + 1];  /// 2 Array
     for(int i = 1; i <= n1; i++)
     {
         L[i] = A[p + i - 1];
@@ -19,6 +19,7 @@ void merges(int p , int q, int r)
     L[n1 + 1] = inf;
     R[n2 + 1] = inf;
     int i = 1, j = 1;
+    /// Mering two array
     for(int k = p; k <= r; k++)
     {
         if(L[i] <= R[j])
@@ -38,6 +39,7 @@ void mergesort(int p , int r)
     if(p < r)
     {
         int q = ((p + r) / 2);
+        /// recursive call 
         mergesort(p , q);
         mergesort(q + 1,  r);
         merges(p , q, r);
